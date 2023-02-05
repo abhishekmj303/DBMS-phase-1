1baac8158cd2682c389a568f9e056d35660b0836
Abhishek M J <abhishekmj303@gmail.com>
2023-02-05 10:54:10 +0530

    Fix browser back-button behaviour

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cards = document.querySelectorAll('.separate-card');
 const duration = 350;
+var pos = {top: 0, left: 0, width: 0, height: 0};
 
 $("a[href]").click(function (e){
   e.preventDefault();
@@ -20,18 +21,23 @@ const onCardClick = async (e) => {
     cardClone.classList.add('card-expanded');
     // get the location of the card in the view
     const {top, left, width, height} = card.getBoundingClientRect();
+    pos.top = top+"px";
+    pos.left = left+"px";
+    pos.width = width+"px";
+    pos.height = height+"px";
     // position the clone on top of the original
     cardClone.style.position = 'fixed';
-    cardClone.style.top = top + 'px';
-    cardClone.style.left = left + 'px';
-    cardClone.style.width = width + 'px';
-    cardClone.style.height = height + 'px';
+    cardClone.style.top = pos.top;
+    cardClone.style.left = pos.left;
+    cardClone.style.width = pos.width;
+    cardClone.style.height = pos.height;
     // hide the original card with opacity
-    card.style.opacity = '0';
+    // card.style.opacity = '0';
     // add card to the body tag
     document.body.appendChild(cardClone);
     // expand the clone card
     await toggleExpansion(cardClone, {top: "-4px", left: "-4px", width: '100vw', height: '100vh'});
+    // document.body.removeChild(cardClone);
 };
 
 const toggleExpansion = (element, to) => {
@@ -50,3 +56,16 @@ return new Promise((res) => {
     })
 }
 cards.forEach(card => card.addEventListener('click', onCardClick));
+
+window.addEventListener( "pageshow", async function ( event ) {
+  var historyTraversal = event.persisted ||
+                         ( typeof window.performance != "undefined" &&
+                              window.performance.navigation.type === 2 );
+  if ( historyTraversal ) {
+    // Handle page restore.
+      cardClone = document.querySelector('.outer-card');
+      await toggleExpansion(cardClone, pos);
+      document.body.removeChild(cardClone);
+      pos = {top: 0, left: 0, width: 0, height: 0};
+  }
+});
