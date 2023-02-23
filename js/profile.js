293262d2cef44bdd762dbca04c4c798eb09e5c94
Abhishek M J <abhishekmj303@gmail.com>
2023-02-24 03:00:33 +0530

    Add animations and reorder files  - add fluid cursor animation  - change to butter.js  - keep js and css files separately

--- js/profile.js
+++ js/profile.js
@@ -60,40 +60,40 @@ function disableSelection(element) {
 
 
 // SmoothScroll
-'use strict';
-
-disableScroll();
-if (!isTouchDevice) smoothScroll();
-
-window.onresize = () => {
-  resizeBodyHeight();
-};
-
-window.onload = () => {
-  enableScroll();
-  resizeBodyHeight();
-};
-
-// Functions
-
-function disableScroll() {
-  document.body.style.overflow = 'hidden';
-}
-
-function enableScroll() {
-  document.body.style.overflow = '';
-}
-
-function smoothScroll() {
-  document.querySelector('.viewport').classList.add('SmoothScroll');
-
-  new SmoothScroll({
-    target: document.querySelector('.container'),
-    scrollEase: 0.08,
-    maxOffset: 500,
-  });
-}
-
-function resizeBodyHeight() {
-  document.body.style.height = document.querySelector('.viewport').scrollHeight + 'px';
-}
+// 'use strict';
+//
+// disableScroll();
+// if (!isTouchDevice) smoothScroll();
+//
+// window.onresize = () => {
+//   resizeBodyHeight();
+// };
+//
+// window.onload = () => {
+//   enableScroll();
+//   resizeBodyHeight();
+// };
+//
+// // Functions
+//
+// function disableScroll() {
+//   document.body.style.overflow = 'hidden';
+// }
+//
+// function enableScroll() {
+//   document.body.style.overflow = '';
+// }
+//
+// function smoothScroll() {
+//   document.querySelector('.viewport').classList.add('SmoothScroll');
+//
+//   new SmoothScroll({
+//     target: document.querySelector('.container'),
+//     scrollEase: 0.08,
+//     maxOffset: 500,
+//   });
+// }
+//
+// function resizeBodyHeight() {
+//   document.body.style.height = document.querySelector('.viewport').scrollHeight + 'px';
+// }
