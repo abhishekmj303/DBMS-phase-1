e8e97635d28f2470c4b179a7e976bd149256a90a
Abhishek M J <abhishekmj303@gmail.com>
2023-02-24 23:12:11 +0530

    Change back to SmoothScroll.js

--- js/profile.js
+++ js/profile.js
@@ -59,41 +59,41 @@ function disableSelection(element) {
 }
 
 
-// SmoothScroll
-// 'use strict';
-//
-// disableScroll();
-// if (!isTouchDevice) smoothScroll();
-//
-// window.onresize = () => {
-//   resizeBodyHeight();
-// };
-//
-// window.onload = () => {
-//   enableScroll();
-//   resizeBodyHeight();
-// };
-//
-// // Functions
-//
-// function disableScroll() {
-//   document.body.style.overflow = 'hidden';
-// }
-//
-// function enableScroll() {
-//   document.body.style.overflow = '';
-// }
-//
-// function smoothScroll() {
-//   document.querySelector('.viewport').classList.add('SmoothScroll');
-//
-//   new SmoothScroll({
-//     target: document.querySelector('.container'),
-//     scrollEase: 0.08,
-//     maxOffset: 500,
-//   });
-// }
-//
-// function resizeBodyHeight() {
-//   document.body.style.height = document.querySelector('.viewport').scrollHeight + 'px';
-// }
+SmoothScroll
+'use strict';
+
+disableScroll();
+if (!isTouchDevice) smoothScroll();
+
+window.onresize = () => {
+  resizeBodyHeight();
+};
+
+window.onload = () => {
+  enableScroll();
+  resizeBodyHeight();
+};
+
+// Functions
+
+function disableScroll() {
+  document.body.style.overflow = 'hidden';
+}
+
+function enableScroll() {
+  document.body.style.overflow = '';
+}
+
+function smoothScroll() {
+  document.querySelector('.viewport').classList.add('SmoothScroll');
+
+  new SmoothScroll({
+    target: document.querySelector('.container'),
+    scrollEase: 0.08,
+    maxOffset: 500,
+  });
+}
+
+function resizeBodyHeight() {
+  document.body.style.height = document.querySelector('.viewport').scrollHeight + 'px';
+}
